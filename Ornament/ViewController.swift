39538de407ffd695f22f0bd126513fe2ec2f4449
AleKuv55 <kuznetsov.alexsandr@phystech.edu>
2021-11-14 14:34:18 +0300

    Initial Commit

---
+++ Ornament/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ornament
+//
+//  Created by Alexandr Kuznetsov on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
